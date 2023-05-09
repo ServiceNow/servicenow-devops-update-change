9861531a1466c16671ae07a2504542d722d9fc28
meera.gajawada <meera.gajawada@servicenow.com>
2023-05-09 19:59:50 +0530

    STRY55387875: Added changes to support token authentication for change update action

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ const main = async() => {
             let response;
             let httpHeaders;
             if(!devopsIntegrationToken && !username && !passwd){
-                displayErrorMsg('Either secret token or integration username, password is needed for integration user authentication');
+                core.setFailed('Either secret token or integration username, password is needed for integration user authentication');
                 return;
             }else if(devopsIntegrationToken){
                 const defaultHeadersv2 = {
@@ -75,7 +75,7 @@ const main = async() => {
                 httpHeaders = { headers: defaultHeadersv1 };
                 restendpoint = endpointv1;
             }else{
-                displayErrorMsg('For Basic Auth, Username and Password is mandatory for integration user authentication');
+                core.setFailed('For Basic Auth, Username and Password is mandatory for integration user authentication');
                 return;
             }
             response = await axios.put(restendpoint, changeRequestDetailsStr, httpHeaders);
